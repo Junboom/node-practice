6623fd220ed7d163a32bdace92c65072790b764b
junboom <ticktock0414@naver.com>
2019-12-26 15:45:29 +0900

    node simple test

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# node-practice 
+# node-practice
+
+## 서버를 구동하고 curl로 요청하기
+
+> curl -X GET '127.0.0.1:3000'
+
+
+## 포트를 확인할 때
+
+> telnet localhost 80
+> telnet localhost 3000
+
+
+## 서버 주소를 알아낼 때
+
+> nslookup localhost
+
+
+## 테스트 코드
+
+> npm i mocha --save-dev
--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const http = require('http');
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// 
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+// 
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/', (req, res) => {
+  res.send('Hello World!\n');
+ });
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use('/users', require('./api/user'));
+
+module.exports = app;
